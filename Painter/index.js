7032ff5031fbbeebc67872a7d368e36ad5a8c548
PrathameshThorat008 <thoratprathamesh008@gmail.com>
2022-03-08 21:14:27 +0530

    BG-Color and Download added in Painter

--- Painter/index.js
+++ Painter/index.js
@@ -57,7 +57,15 @@ const Tend = (e) => {
   ctx.closePath();
 };
 
+const toHex = (val) => {
+  let [a, b] = [val[0], val[1]];
+
+  return parseInt(val, 16);
+};
+
 window.onload = () => {
+  // alert("Best for PC");
+  let bg = "#ffffff";
   canvas = document.getElementById("box");
   ctx = canvas.getContext("2d");
 
@@ -67,8 +75,46 @@ window.onload = () => {
   document.getElementById("color").onchange = (e) => {
     ctx.strokeStyle = e.target.value;
   };
+  document.getElementById("bgcolor").onchange = (e) => {
+    bg = e.target.value;
+    canvas.style.backgroundColor = bg;
+  };
   document.getElementById("clear").onclick = (e) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+  document.getElementById("download").onclick = (e) => {
+    let a = document.createElement("canvas");
+    a.width = canvas.width;
+    a.height = canvas.height;
+
+    let actx = a.getContext("2d");
+
+    actx.fillStyle = bg;
+    actx.fillRect(0, 0, a.width, a.height);
+
+    n = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < n.data.length; i += 4) {
+      if (n.data[i + 3] > 0) continue;
+
+      n.data[i] = toHex(bg[1] + bg[2]);
+      n.data[i + 1] = toHex(bg[3] + bg[4]);
+      n.data[i + 2] = toHex(bg[5] + bg[6]);
+      n.data[i + 3] = 255;
+    }
+
+    actx.putImageData(n, 0, 0);
+
+    actx.canvas.toBlob((blob) => {
+      let url = URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "image.jpg");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }, "image/jpeg");
   };
 
   window.onresize = setSize;
